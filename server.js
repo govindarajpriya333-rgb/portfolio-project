0d971ef14a7785500c2c380c46836c7440d41a53
25dtsa23-gps <25dtsa23@kristujayanti.com>
2026-03-27 07:33:33 +0530

    fixed server crash

--- server.js
+++ server.js
@@ -1,48 +1,26 @@
 const express = require("express");
-const mysql = require("mysql2");
 const cors = require("cors");
-contst status = document.getElementById("status");
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// MySQL connection
-const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "Priyalaxmi@2008",
-  database: "portfolio_db"
-});
-
-db.connect(err => {
-  if (err) {
-    console.log("❌ DB Error:", err);
-  } else {
-    console.log("✅ MySQL Connected");
-  }
-});
-
-// API
-app.post("/contact", (req, res) => {
+app.post("/", (req, res) => {
   const { name, email, message } = req.body;
 
-  const sql = "INSERT INTO contacts (name, email, message) VALUES (?, ?, ?)";
-  
-  db.query(sql, [name, email, message], (err, result) => {
-    if (err) {
-      console.log(err);
-      return res.status(500).send("Error");
-    }
-    res.send("Success");
-  });
+  console.log("New Message:");
+  console.log(name, email, message);
+
+  res.status(200).json({ message: "Success" });
 });
 
-app.listen(3000, () => {
-  console.log("Server running on http://localhost:3000");
+app.get("/", (req, res) => {
+  res.send("Server is running");
 });
-if (response.ok) {
-  status.innerText = "✅ Message sent successfully!";
-} else {
-  status.innerText = "❌ Failed to send message";
-}
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(Server running on port ${PORT});
+});
