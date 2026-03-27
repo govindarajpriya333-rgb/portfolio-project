e1827855d2428b36a8e6122163496ba4fd3c328e
govindarajpriya333-rgb <govindarajpriya333@gmail.com>
2026-03-27 08:38:04 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -14,10 +14,10 @@ app.get("/", (req, res) => {
 // ✅ FIX: Contact route
 app.post("/", (req, res) => {
   console.log(req.body);
-  res.status(200).json({ message: "Message received" });
+res.json({ message: "Message received" });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 
 app.listen(PORT, () => {
   console.log("Server running on port " + PORT);
